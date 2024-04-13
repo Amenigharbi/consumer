7a9b3466c773b1e6f485f0d15f267fcddff619d4
ameni <gharbiameni17@gmail.com>
2024-04-13 19:25:23 +0100

    new updates

--- src/main/java/com/Microservices/ConsumerService/Aspect/LoggingAspect.java
+++ src/main/java/com/Microservices/ConsumerService/Aspect/LoggingAspect.java
@@ -15,20 +15,20 @@ public class LoggingAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Before("execution(* com.Microservices.StockService.Service.*.*(..))")
+    @Before("execution(* com.Microservices.ConsumerService.Service.*.*(..))")
     public void logBeforeServiceMethods(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
         logger.info("Method '{}' called with arguments: {}", methodName, args);
     }
 
-    @AfterReturning(pointcut = "execution(* com.Microservices.StockService.Service.*.*(..))", returning = "result")
+    @AfterReturning(pointcut = "execution(* com.Microservices.ConsumerService.Service.*.*(..))", returning = "result")
     public void logAfterReturningServiceMethods(JoinPoint joinPoint, Object result) {
         String methodName = joinPoint.getSignature().getName();
         logger.info("Method '{}' returned: {}", methodName, result);
     }
 
-    @AfterThrowing(pointcut = "execution(* com.Microservices.StockService.Service.*.*(..))", throwing = "exception")
+    @AfterThrowing(pointcut = "execution(* com.Microservices.ConsumerService.Service.*.*(..))", throwing = "exception")
     public void logAfterThrowingServiceMethods(JoinPoint joinPoint, Throwable exception) {
         String methodName = joinPoint.getSignature().getName();
         logger.error("Method '{}' threw an exception: {}", methodName, exception.getMessage());
